5ac93e610ea4401c1fc5a04e714146bee6ef37ef
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-04-19 15:37:16 -0300

    refactor: Refactor CreateTagForm to use react-query for mutation

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as Dialog from "@radix-ui/react-dialog";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { Button } from "./ui/button";
 
@@ -11,6 +12,8 @@ const createTagSchema = z.object({
 });
 
 export function CreateTagForm() {
+  const queryClient = useQueryClient();
+
   const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>(
     {
       resolver: zodResolver(createTagSchema),
@@ -30,22 +33,33 @@ export function CreateTagForm() {
 
   const slug = watch("title") ? getSlugFromString(watch("title")) : "";
 
-  async function createaTag({ title }: CreateTagSchema) {
-    // delay 2s
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ title }: CreateTagSchema) => {
+      // delay 2s
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    await fetch("http://localhost:3333/tags", {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        slug,
-        amountOfVideos: 0,
-      }),
-    });
+      await fetch("http://localhost:3333/tags", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          slug,
+          amountOfVideos: 0,
+        }),
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["get-tags"],
+      });
+    },
+  });
+
+  async function createTag({ title }: CreateTagSchema) {
+    await mutateAsync({ title });
   }
 
   return (
-    <form onSubmit={handleSubmit(createaTag)} className="w-full space-y-6">
+    <form onSubmit={handleSubmit(createTag)} className="w-full space-y-6">
       <div className="space-y-2">
         <label className="text-sm font-medium block" htmlFor="title">
           Tag name
