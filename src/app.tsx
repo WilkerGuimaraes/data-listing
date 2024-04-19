f1007fa2e5132ee42a2e760adcbad10ff4e0632e
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-04-19 14:07:02 -0300

    feat: add form ui component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import { Tabs } from "./components/tabs";
 import { Header } from "./components/header";
@@ -16,6 +17,7 @@ import {
 } from "./components/ui/table";
 import { Pagination } from "./components/pagination";
 import { useState } from "react";
+import { CreateTagForm } from "./components/create-tag-form";
 
 export interface TagResponse {
   first: number;
@@ -78,10 +80,32 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="fixed p-10 space-y-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create Tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts
+                  </Dialog.Description>
+                </div>
+
+                <CreateTagForm />
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
 
         <div className="flex items-center justify-between">
---
+++ src/components/create-tag-form.tsx
@@ -0,0 +1,43 @@
+import { Check, X } from "lucide-react";
+
+import { Button } from "./ui/button";
+
+export function CreateTagForm() {
+  return (
+    <form className="w-full space-y-6">
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="name">
+          Tag name
+        </label>
+        <input
+          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+          id="name"
+          type="text"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="slug">
+          Slug
+        </label>
+        <input
+          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+          id="slug"
+          type="text"
+          readOnly
+        />
+      </div>
+
+      <div className="flex items-center justify-end gap-2">
+        <Button>
+          <X className="size-3" />
+          Cancel
+        </Button>
+        <Button className="bg-teal-400 text-teal-950" type="submit">
+          <Check className="size-3" />
+          Save
+        </Button>
+      </div>
+    </form>
+  );
+}
